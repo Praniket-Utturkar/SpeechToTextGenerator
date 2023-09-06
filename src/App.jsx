3b0152d603f192baf8ac5dd558cd49aa55e7cdc5
Praniket <praniketutturkane4444@gmail.com>
2023-09-06 19:24:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import 'regenerator-runtime/runtime'
+import useClipboard from "react-use-clipboard";
+
+const App = () => {
+  const [textToCopy , setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy);
+ const startListening = () => SpeechRecognition.startListening({ continuous: true,language:'en-IN'  })
+
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
+
+  return (
+    <>
+    <div className="container">
+      <h2>Speech to Text Generator</h2>
+      <br />
+      <p>&quot;Transcribe Your Thoughts: Your Voice, Our Words&quot;</p>
+      <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+         {transcript}
+      </div>
+      <div className="btn-style">
+      <button onClick={setCopied}>
+      {isCopied ? 'Copied!' : 'Copy to clipboard'}
+    </button>
+        <button onClick={startListening}>Start Listening</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default App;
