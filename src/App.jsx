5c588b1d8f9b43afed233c5b39ed79f3d5885fa0
Praniket <praniketutturkane4444@gmail.com>
2024-07-09 12:00:33 +0530

    Final phase 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import "./App.css"
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import 'regenerator-runtime/runtime'
