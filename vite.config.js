fd365892320a5140e66181d868afd8d5d5849927
Praniket <praniketutturkane4444@gmail.com>
2024-07-09 12:13:37 +0530

    Final phase 3

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
-  ]
-});
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
+
+})
