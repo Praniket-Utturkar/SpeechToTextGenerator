97a39e3e838cd46e8d3d15a6c458c14d1103402f
Praniket <praniketutturkane4444@gmail.com>
2024-07-09 12:17:52 +0530

    Final phase 4

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
-
-})
+  base: import.meta.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
+  define: {
+    'process.env': {}
+  }
+});
