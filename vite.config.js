007813becbbb31c71c34fd336d4334eb916b34a9
Praniket <praniketutturkane4444@gmail.com>
2024-07-09 12:42:29 +0530

    Final phase 7

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
-  ],
-  base: import.meta.env.VITE_BASE_PATH || '/',
-  define: {
-    'process.env': {}
-  }
-});
+  plugins: [react()],
+})
